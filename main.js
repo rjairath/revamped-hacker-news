728692c128a2903ddd7377fcdd333799007bf1a8
rishabh jairath <rishabhjairath@gmail.com>
2019-02-12 17:36:43 +0530

    js file structure added

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-"main.js" 
+const state = {};
+const baseUrl = "https://hacker-news.firebaseio.com/v0";
+
+function fetchTopStories(){
+	const topStoriesUrl = `${hnBaseUrl}/topstories.json`;
+	return fetch(topStoriesUrl).then(response=> response.json())
+		.then()
+}
+function fetchStories(){
+
+}
+
+function renderStories(){
+
+}
+
+function toggleStoryText(){
+
+}
+function fetchComments(){
+
+}
+function fetchOrToggleComments(){
+
+}
+function ToggleComment(){
+
+}
+
+function renderComments(){
+
+}
+fetchTopStories();
