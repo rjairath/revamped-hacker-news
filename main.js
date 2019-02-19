9fe34163fc07db73cdc5425f849b61352040ecf5
rishabh jairath <rishabhjairath@gmail.com>
2019-02-19 15:51:19 +0530

    toggle added

--- main.js
+++ main.js
@@ -103,5 +103,8 @@ function renderComments(comments, storyId){
 fetchTopStories();
 
 function toggleDarkLight(){
-	
+	var body = document.getElementById("body");
+	var currentClass = body.className;
+
+	body.className = (currentClass === "dark-mode"? "light-mode" : "dark-mode");
 }
