a5b645e059d2d91ebf5a3061968446d4251892a3
rishabh jairath <rishabhjairath@gmail.com>
2019-02-25 18:15:49 +0530

    js file edited

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ function fetchTopStories(){
 }
 function fetchStories(data){
 	let topStories = data.slice(0, 30);
-	let storyIds = topStories.map((storyId)=>{
+	let storyDetails = topStories.map((storyId)=>{
 		const storyUrl = `${baseUrl}/item/${storyId}.json`;
 		return fetch(storyUrl).then((response)=> response.json());
 	});
-	return Promise.all(storyIds).then((stories)=>{
+	return Promise.all(storyDetails).then((stories)=>{
 		state.stories = stories;
 		renderStories(stories);
 	});
@@ -22,14 +22,17 @@ function renderStories(stories){
 	return stories.map((story)=>{
 		const userUrl = `https://news.ycombinator.com/user?id=${story.by}`;
 	    const storyItemUrl = `https://news.ycombinator.com/item?id=${story.id}`;
+
 	    const html = `
 	      <div class='story' id='${story.id}'>
 	        <h3 class='title'>
 	          ${story.url ? `<a href='${story.url}' target='_blank'>${story.title}</a>`
 	            : `<a href='javascript:void(0)' onclick="toggleStoryText('${story.id}')" >${story.title}</a>`}
 	        </h3>
+
 	        <span class='score'> ${story.score} </span> points by
 	        <a href='${userUrl}' target='_blank' class='story-by'> ${story.by}</a>
+
 	        <div class='toggle-view'>
 	          ${story.kids ? `
 	            <span
@@ -37,17 +40,18 @@ function renderStories(stories){
 	              class='comments'
 	            > [toggle ${story.descendants} comments] </span>`
 	          : '' }
-	          <a href='${storyItemUrl}' target='_blank' class='hnLink'>[view on HN]</a>
 	        </div>
+
 	        ${story.text ?
 	          `<div class='storyText' id='storyText-${story.id}' style='display:none;'>
 	            ${story.text} </div>` : '' }
+
 	        <div id='comments-${story.id}' style='display: block;'></div>
 	      </div> `
     	document.getElementById('hn').insertAdjacentHTML('beforeend', html);
 	})
 }
-
+//Toggles the story text if there is no story url.
 function toggleStoryText(){
 	const storyText = document.getElementById(`storyText-${story.id}`);
 	storyText.style.display = (storyText.style.display === "block")? "none": "block";
@@ -56,8 +60,8 @@ function fetchComments(kids, storyId){
 	const commentIds = kids.split(',')
 	const allComments = commentIds.map((commentId) => {
 		const commentUrl = `${baseUrl}/item/${commentId}.json`
-		return fetch(commentUrl).then((response) => response.json()).then((comment) => comment)
-	})
+		return fetch(commentUrl).then((response) => response.json());
+	});
 	return Promise.all(allComments).then((comments) => {
 		state[storyId] = comments;
 		renderComments(comments, storyId);
@@ -76,7 +80,7 @@ function ToggleComment(){
 	comment.style.display = (comment.style.display === 'block') ? 'none' : 'block'
 	toggle.innerHTML = (toggle.innerHTML === '[ - ]') ? '[ + ]' : '[ - ]'
 }
-
+//Call this function after all comments for a story ID have been fetched.
 function renderComments(comments, storyId){
 	return comments.map((comment) => {
 		const userUrl = `https://news.ycombinator.com/user?id=${comment.by}`;
